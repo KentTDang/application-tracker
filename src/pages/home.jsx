9ff849e19288ba76500e962e30351c057d1e5a62
Kent Dang <kenttdang@gmail.com>
2024-03-16 18:07:29 -0400

    Connect firebase to project - Able to create collections

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,26 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
+import { firestore } from "../firebase";
+import { addDoc,collection } from "@firebase/firestore";
 
 export default function Home() {
 
   const messageRef = useRef();
+  const ref = collection(firestore,"job-applications");
   const handleSave = async(e) => {
+    
     e.preventDefault();
     console.log(messageRef.current.value);
+
+    let data = {
+      message: messageRef.current.value,
+    }
+
+    try {
+      addDoc(ref,data);
+    }catch(e){
+      console.log(e);
+    }
+
   };
 
   return <div>
