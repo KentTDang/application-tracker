c3c3a797fac40286b95ea44b4f11a0272fb59a80
Kent Dang <kenttdang@gmail.com>
2024-03-18 19:08:55 -0400

    Try catch for non-matching passwords and already existing user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Home from "./pages/home";
-import SignUp from './pages/signup';
+import SignUp from './pages/Signup';
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../src/contexts/AuthContext"
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: '400px '}}><SignUp/></div>
-    </Container>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: '400px ' }}><SignUp /></div>
+      </Container>
+    </AuthProvider>
+
   )
 }
 
