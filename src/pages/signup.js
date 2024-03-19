f056eaffe0477f9cbb52459e1421be2ed218eb28
Kent Dang <kenttdang@gmail.com>
2024-03-19 16:25:00 -0400

    Navigate to dashboard after log in/ sign in

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext';
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function SignUp() {
 
@@ -11,6 +11,7 @@ export default function SignUp() {
   const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -23,6 +24,7 @@ export default function SignUp() {
       setError("")
       setLoading(true)
       await signup(emailRef.current.value, passwordRef.current.value)
+      navigate("/")
     } catch {
       setError("Failed to create an account")
     }
