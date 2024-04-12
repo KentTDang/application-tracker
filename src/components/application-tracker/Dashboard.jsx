44ff23296220fbb84f36c17c933820274c235780
Kent Dang <kenttdang@gmail.com>
2024-04-12 01:25:40 -0400

    Implement react popup

--- src/components/application-tracker/Dashboard.jsx
+++ src/components/application-tracker/Dashboard.jsx
@@ -80,7 +80,7 @@ export default function Dashboard() {
   const handleSave = async (e) => {
 
     e.preventDefault();
-    
+
     let data = {
       company: companyRef.current.value,
       stage: stageRef.current.value,
@@ -105,6 +105,13 @@ export default function Dashboard() {
     <>
       <div className="header">
         Job Application Tracker
+        
+        <div className="logout-container">
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <button variant="link" onClick={handleLogout}>Log Out</button>
+        </div>
       </div>
       <div className="profile-container">
 
@@ -146,24 +153,7 @@ export default function Dashboard() {
         </div>
 
       </div>
-      <Container className="d-flex align-items-center justify-content-center"
 
-        style={{ minHeight: "100vh" }}>
-        <div className="profile-container">
-          <Card >
-            <Card.Body>
-              <h2 className="text-center mb-4">Profile</h2>
-              {error && <Alert variant="danger">{error}</Alert>}
-              <strong>Email:</strong> {currentUser.email}
-              <Link to='/update-profile' className="btn btn-primary w-100 mt-3">Update Profile</Link>
-            </Card.Body>
-          </Card>
-          <div className="w-100 text-center mt-2">
-            <Button variant="link" onClick={handleLogout}>Log Out</Button>
-          </div>
-        </div>
-
-      </Container >
     </>
 
   )
